180e95d2d36c4d04198de770526c95bb2605f078
Ha Yen <hanguyentieuyen@gmail.com>
2023-12-01 00:06:11 +0700

    submit login form with crendentials

--- simple-note/src/lib/authOptions.ts
+++ simple-note/src/lib/authOptions.ts
@@ -2,6 +2,7 @@ import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials"
 import { db } from "./db";
+import { compare } from "bcrypt";
 
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(db),
@@ -16,18 +17,31 @@ export const authOptions: NextAuthOptions = {
           name: 'Credentials',
           
           credentials: {
-            username: { label: "Email", type: "email", placeholder: "yen@gmail.com" },
+            email: { label: "Email", type: "email", placeholder: "yen@gmail.com" },
             password: { label: "Password", type: "password" }
           },
           async authorize(credentials, req) {
-            const user = { id: "1", name: "Yen Ha", email: "yen@gmail.com" }
-      
-            if (user) {
-              return user
-            } else {
-              return null
-      
-            }
+           if(!credentials?.email || !credentials.password) {
+            return null
+           }
+           const existedUser = await db.user.findUnique({
+            where: {email: credentials.email}
+           });
+
+           if(!existedUser) {
+            return null
+           }
+
+           const passwordMatch = await compare(credentials.password, existedUser.password)
+           if(!passwordMatch) {
+            return null
+           }
+
+           return {
+            id: `${existedUser.id}`,
+            username: existedUser.username,
+            email: existedUser.email
+           }
           }
         })
       ]
